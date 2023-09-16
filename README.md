9463ac85388f3af4277120c6de87c332225d3384
Andrew Healey <doolie.healey@gmail.com>
2023-09-16 16:00:50 -0700

    Show how to pass coords

--- README.md
+++ README.md
@@ -9,3 +9,26 @@ This was wrong.
 
 You should clone this repo with `git clone https://github.com/llama2d/llama2d.git --recursive` to get the transformers submodule.
 Then `cd transformers && pip install -e .`
+
+
+# LlamaForCausalLM
+
+This is my modified Llama class.
+
+It can be trained and inferenced.
+
+To inference with coords, run `model.generate(..., coords=coords)`, where `coords` is a list of 2D coordinates.
+
+To train with coords, return a dictionary like so:
+
+```python
+class MyDataset(Dataset):
+    def __getitem__(self, idx):
+        ...
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "labels": labels,
+            "coords": coords,
+        }
+```
--- llama2d/pos_embeds/stolen_code.py
+++ llama2d/pos_embeds/stolen_code.py
@@ -23,6 +23,12 @@ class PositionEmbeddingRandom(nn.Module):
         # 0 is for not a point, 1 is for a point
         self.is_a_point_embed = nn.Embedding(2, num_pos_feats)
 
+
+        # TODO: for a sanity check, freeze this param to zero.
+        # this will test if a normal Llama can learn to beat WebArena.
+        # a gate for the positional encoding
+        self.lbd = nn.Parameter(torch.tensor(0.0))
+
     def _pe_encoding(self, coords: torch.Tensor) -> torch.Tensor:
         """Positionally encode points that are normalized to [0,1]."""
         # assuming coords are in [0, 1]^2 square and have d_1 x ... x d_n x 2 shape
@@ -56,30 +62,31 @@ class PositionEmbeddingRandom(nn.Module):
 
 
     def apply_rotary_2d_pos_emb(self,q,k,coords):
-        # shape of q and k: [bs, ???, seq_len, dim]
-        # aka: B x ??? x N x C
-        # shape of coords: [bs, ???, seq_len, 2]
+        # shape of q and k: [bs, num_heads, seq_len, dim]
+        # aka: B x num_heads x N x C
+        # shape of coords: [bs, seq_len, 2]
 
         # maybe ??? is 1:
         assert q.shape[1] == 1,f"The ??? dimension of q is {q.shape[1]}, not 1"
         assert k.shape[1] == 1,f"The ??? dimension of k is {k.shape[1]}, not 1"
-        assert len(coords.shape) == 4,f"The shape of coords should have dims (bs,???,seq_len,2)"
-        assert coords.shape[1] == 1,f"The ??? dimension of coords is {coords.shape[1]}, not 1"
+        assert len(coords.shape) == 3,f"The shape of coords should have dims (batch_size,seq_len,2)"
 
-        bs,_,seq_len,dim = q.shape
+        bs,num_heads,seq_len,dim = q.shape
 
         # some coords will be [-1,-1] because they have no known position
         # we should not add these coords to the positional embedding
         is_a_point = coords[:,:,0] != -1
 
-        pos_embeds = self.forward_with_coords(coords.squeeze(1),image_size=SCREEN_RESOLUTION)
+        pos_embeds = self.forward_with_coords(coords,image_size=SCREEN_RESOLUTION)
         assert pos_embeds.shape == (bs,seq_len,dim)
 
         is_a_point_embeds = self.is_a_point_embed(is_a_point.long())
         assert is_a_point_embeds.shape == (bs,seq_len,dim)
 
+        delta = pos_embeds.unsqueeze(1) + is_a_point_embeds.unsqueeze(1)
+
         # add the positional embedding to the query and key
-        q = q + pos_embeds.unsqueeze(1)
-        k = k + pos_embeds.unsqueeze(1)
+        q = q + delta * self.lbd
+        k = k + delta * self.lbd
 
         return q,k,is_a_point_embeds
