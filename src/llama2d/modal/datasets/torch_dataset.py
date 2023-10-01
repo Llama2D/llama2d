3de2d85e0730186d19486afc42b64f32ca76ae60
Andrew Healey <doolie.healey@gmail.com>
2023-10-01 16:12:56 +0100

    Add keep_fraction to torch_dataset

--- src/llama2d/modal/datasets/torch_dataset.py
+++ src/llama2d/modal/datasets/torch_dataset.py
@@ -5,8 +5,9 @@ dataset_registry = {
 }
 
 def get_custom_dataset(dataset_config, tokenizer, split):
-    train_size = 5000
-    val_size = 1000
+    keep_fraction=dataset_config.keep_fraction
+    train_size = int(5000*keep_fraction)
+    val_size = int(1000*keep_fraction)
     return Llama2dZooCompassDataset(
         num_screens=train_size if split == "train" else val_size,
         words_per_screen=20,
