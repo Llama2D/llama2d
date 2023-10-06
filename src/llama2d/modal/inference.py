9c1920d4e04dd4a17d67f5900dc0df97f08995a9
Sarma Tangirala <vttangir@amazon.com>
2023-10-06 11:12:56 -0700

    Setup coords and adpater

--- src/llama2d/modal/inference.py
+++ src/llama2d/modal/inference.py
@@ -4,6 +4,11 @@ import subprocess
 from common import BASE_MODELS, VOLUME_CONFIG, stub
 from modal import Image, gpu, method
 
+from loguru import logger
+import torch
+
+from transformers import LlamaConfig, LlamaForCausalLM, AutoTokenizer
+
 tgi_image = (
     Image.from_registry("ghcr.io/huggingface/text-generation-inference:1.0.3")
     .dockerfile_commands("ENTRYPOINT []")
@@ -13,15 +18,51 @@ tgi_image = (
 )
 
 
+def _download_and_unload_peft(model_id, revision, trust_remote_code):
+    """
+    from: https://github.com/huggingface/text-generation-inference/blob/00b8f36fba62e457ff143cce35564ac6704db860/server/text_generation_server/utils/peft.py#L10
+    """
+    torch_dtype = torch.float16
+
+    logger.info("Peft model detected.")
+    logger.info("Loading the model it might take a while without feedback")
+
+    model = LlamaForCausalLM.from_pretrained(
+        model_id,
+        revision=revision,
+        torch_dtype=torch_dtype,
+        trust_remote_code=trust_remote_code,
+        low_cpu_mem_usage=True,
+    )
+
+    logger.info(f"Loaded.")
+    logger.info(f"Merging the lora weights.")
+
+    model = model.merge_and_unload()
+
+    os.makedirs(model_id, exist_ok=True)
+    cache_dir = model_id
+    logger.info(f"Saving the newly created merged model to {cache_dir}")
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.add_special_tokens(
+        {
+            "pad_token": "<PAD>",
+        }
+    )
+
+    model.save_pretrained(cache_dir, safe_serialization=True)
+    model.config.save_pretrained(cache_dir)
+    tokenizer.save_pretrained(cache_dir)
+
+
 @stub.function(image=tgi_image, volumes=VOLUME_CONFIG, timeout=60 * 20)
 def merge(run_id: str, commit: bool = False):
-    from text_generation_server.utils.peft import download_and_unload_peft
-
     os.mkdir(f"/results/{run_id}/merged")
     subprocess.call(f"cp /results/{run_id}/*.* /results/{run_id}/merged", shell=True)
 
     print(f"Merging weights for fine-tuned {run_id=}.")
-    download_and_unload_peft(f"/results/{run_id}/merged", None, False)
+    _download_and_unload_peft(f"/results/{run_id}/merged", None, False)
 
     if commit:
         print("Committing merged model permanently (can take a few minutes).")
@@ -64,21 +105,17 @@ class Model:
         self.launcher.terminate()
 
     @method()
-    async def generate(self, prompt: str):
-        result = await self.client.generate(prompt, max_new_tokens=512)
+    async def generate(self, prompt: str, coords):
+        result = await self.client.generate(prompt, coords, max_new_tokens=512)
 
         return result.generated_text
 
 
 @stub.local_entrypoint()
-def main(prompt: str, base: str, run_id: str = "", batch: int = 1):
-    print(f"Running completion for prompt:\n{prompt}")
-
-    print("=" * 20 + "Generating without adapter" + "=" * 20)
-    for output in Model(base).generate.map([prompt] * batch):
-        print(output)
+def main(prompt: str, coords, base: str, run_id: str = "", batch: int = 1,):
+    print(f"Running completion for prompt:\n{prompt} \ncoords{coords}")
 
     if run_id:
         print("=" * 20 + "Generating with adapter" + "=" * 20)
-        for output in Model(base, run_id).generate.map([prompt] * batch):
+        for output in Model(base, run_id).generate.map([prompt] * batch, [coords] * batch):
             print(output)
